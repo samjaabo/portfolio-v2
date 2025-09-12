2250590c31dc9214d611e9c2589f11756832ccbf
Said Amjaabou <mr.amjaabou@gmail.com>
2025-09-12 21:45:27 +0100

    add more spacing

--- src/components/futuristic/dropdown.tsx
+++ src/components/futuristic/dropdown.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Collapsible from "react-collapsible";
 
 export interface DropdownProps {
@@ -12,20 +14,27 @@ export default function Dropdown({
   children = null,
 }: DropdownProps) {
   return (
-    <div className="w-full flex flex-col border-t border-line-color-soft last:rounded-b-xl overflow-hidden">
+    <div className="w-full flex flex-col border-t border-line-color-soft/60 last:rounded-b-xl overflow-hidden">
       {/* Header */}
       <Collapsible
+        id="collapsible-header"
         trigger={
-          <div className="w-full p-4  font-bold cursor-pointer text-xl flex justify-between">
-            <span>{leftTitle}</span>
-            <span>{rightTitle}</span>
+          <div
+            id="collapsible-trigger-1"
+            className="w-full p-8  font-bold cursor-pointer text-xl flex justify-between"
+          >
+            <span id="left-title">{leftTitle}</span>
+            <span id="right-title">{rightTitle}</span>
           </div>
         }
         transitionTime={200}
         // openedClassName="bg-white/25"
       >
         {/* Content */}
-        <div className="p-4 bg-white/40 flex flex-col gap-2 border-t border-line-color-soft ">
+        <div
+          className="p-8 bg-white/40 flex flex-col gap-2 border-t border-line-color-soft/60"
+          id="dropdown-content"
+        >
           {children}
         </div>
       </Collapsible>
