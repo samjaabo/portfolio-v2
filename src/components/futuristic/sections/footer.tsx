16db0d22a39027a5cb8c510c6a6f3bf2cbf6798b
Said Amjaabou <mr.amjaabou@gmail.com>
2025-09-15 21:44:04 +0100

    fix more issue in footer

--- src/components/futuristic/sections/footer.tsx
+++ src/components/futuristic/sections/footer.tsx
@@ -6,8 +6,8 @@ import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <footer className="p-5 mt-32 h-full w-full">
-      <div className="grid grid-cols-1 grid-rows-[repeat(17, minmax(0, auto))] lg:grid-cols-4  lg:grid-rows-[auto_1fr_auto_auto] gap-y-8 gap-x-16 p-8 bg-soft w-full h-fit rounded-xl  text-xs">
+    <footer className=" p-1 sm:p-5 mt-32 h-full w-full">
+      <div className="grid grid-cols-1 grid-rows-[repeat(17, minmax(0, auto))] lg:grid-cols-4  lg:grid-rows-[auto_1fr_auto_auto] gap-y-8 gap-x-16 p-3 sm:p-8 bg-soft w-full h-fit rounded-xl  text-xs">
         {/* row 1 */}
         <div className="flex font-bold text-sm">
           <WebsiteLogo></WebsiteLogo>
@@ -17,7 +17,7 @@ export default function Footer() {
         <div className="items-center uppercase  hidden lg:flex">NEWSLETTER</div>
         {/* row 1 */}
         <div className="flex">
-          <p className="max-lg:max-w-1/2">
+          <p className="sm:max-w-1/2 lg:max-w-full max-w-full">
             Whether you're a beginner or an experienced developer, the course
             will guide you through, principles of motion, timeline animation,
             tools, and techniques used by award-winning developers.
@@ -67,11 +67,11 @@ export default function Footer() {
           <div className=" uppercase  lg:hidden block font-bold">
             NEWSLETTER
           </div>
-          <p className="max-lg:max-w-1/2">
+          <p className="sm:max-w-1/2 lg:max-w-full max-w-full">
             Sign up for our newsletter and get the latest updates, news and
             product offers via email.
           </p>
-          <div className="flex max-lg:max-w-1/2 w-full  ring-0 border-1 border-line-color-soft in-focus-within:border-line-color rounded-xl">
+          <div className="flex sm:max-w-1/2 lg:max-w-full max-w-full w-full  ring-0 border-1 border-line-color-soft in-focus-within:border-line-color rounded-xl">
             <input
               required
               autoComplete="email"
