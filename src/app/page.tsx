5600cd178337552d44de180138a953565c6699a1
Said Amjaabou <mr.amjaabou@gmail.com>
2025-09-19 21:50:07 +0100

    edit in / home page not test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,34 @@ import {
   NavbarContainer,
   NavbarContainerRounded,
   ProfileImageContainer,
+  SmallCardBottomContainer,
   WebsiteLogo,
 } from "@/components/futuristic/svgs";
-import clsx from "clsx";
 import Link from "next/link";
+import clsx from "clsx";
+import HeaderSection from "@/components/futuristic/sections/header";
+import ProjectsSection from "@/components/futuristic/sections/projects";
+import CursusSection from "@/components/futuristic/sections/cursus";
+import Drag from "@/components/futuristic/followPointer";
+import BlogSection from "@/components/futuristic/sections/blog";
+import Footer from "@/components/futuristic/sections/footer";
+import { LiaCodeSolid } from "react-icons/lia";
+import Bg360 from "@/components/futuristic/ui/photoSphereViewer";
 
 export default function TestPage() {
   return (
-    <main className="relative flex flex-col items-center justify-center min-h-screen h-screen bg-white px-2 md:px-8 w-full max-w-[120rem] mx-auto">
-      <div className="bg-warning w-full text-center ">
-        sorry we are in development
-      </div>
-      <div
-        className={clsx(
-          "relative flex h-1/2 w-1/2 bg-soft",
-          "before:w-1/2 before:aspect-square before:content['*'] before:absolute before:-top-1/2 before:translate-y-1/2 before:left-0 before:rotate-0",
-          " before:bg-bg"
-        )}
-      ></div>
+    <main className="relative flex flex-col items-center justify-start min-h-screen h-auto bg-white w-full max-w-[120rem] mx-auto">
+      {/* <div className="bg-warning w-full text-center text-soft px-2 lg:px-8 ">
+        <LiaCodeSolid className="inline-block" /> sorry this website is under
+        development
+      </div> */}
+      <Drag />
+      <HeaderSection />
+      <ProjectsSection />
+      {/* <CursusSection /> */}
+      <BlogSection />
+      <Footer />
+      {/* <div className="md:block hidden fixed bottom-[calc(10px_+_theme(space.8))] right-[calc(10px_+_theme(space.8))] w-8 h-8 rounded-full bg-bg/50 border border-bg"></div> */}
     </main>
   );
 }
